3417fdf44681c19229bc99cebb11e9e8bd19e44a
Alessandro Dutra <cachopaweb@gmail.com>
2020-07-25 12:02:53 -0400

    criado o controller de backlog Sprint

--- UnitController.Backlog.Sprint.pas
+++ UnitController.Backlog.Sprint.pas
@@ -12,13 +12,14 @@ uses
   UnitConexao.FireDAC.Model,
   UnitQuery.FireDAC.Model,
   UnitFactory.Conexao.FireDAC,
-  UnitFuncoesComuns, UnitConstantes;
+  UnitFuncoesComuns, UnitConstantes, UnitBacklog.Produto.Model;
 
 
 type
   TControllerBacklogSprint = class
     class procedure Registrar(App: THorse);
     class procedure Post(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+    class procedure Put(Req: THorseRequest; Res: THorseResponse; Next: TProc);
   end;
 
 implementation
@@ -80,9 +81,61 @@ begin
   end;
 end;
 
+class procedure TControllerBacklogSprint.Put(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  oJson: TJSONObject;
+  Fabrica: iFactoryConexao;
+  Conexao: iConexao;
+  Query: iQuery;
+  Dados: TDataSource;
+  CodigoBS_BP: Integer;
+  CodigoSprint: Integer;
+  BacklogProduto: TBacklogProduto;
+begin
+  // componentes de conexao
+  Fabrica := TFactoryConexaoFireDAC.New;
+  Conexao := Fabrica.Conexao(TConstants.BancoDados);
+  Query := Fabrica.Query(Conexao);
+  Dados := TDataSource.Create(nil);
+  Query.DataSource(Dados);
+  oJson := TJSONObject.Create;
+  if Req.Body <> '' then
+  begin
+    CodigoSprint := Req.Params.Items['id'].ToInteger;
+    BacklogProduto := TBacklogProduto.FromJsonString(Req.Body);
+    try
+      CodigoBS_BP := GeraCodigo('BS_BP', 'BB_CODIGO');
+      Query.Add('INSERT INTO BS_BP (BB_CODIGO, BB_BP, BB_BS, BB_PRIORIDADE)');
+      Query.Add('VALUES (:CODIGO, :BP, :BS, :PRIORIDADE);');
+      Query.AddParam('CODIGO', CodigoBS_BP);
+      Query.AddParam('BP', BacklogProduto.Codigo);
+      Query.AddParam('BS', CodigoSprint);
+      Query.AddParam('PRIORIDADE', BacklogProduto.Necessidade);
+      Query.ExecSQL;
+      oJson.AddPair('BS_BP', CodigoBS_BP.ToString);
+      Res.Send<TJSONObject>(oJson).Status(THTTPStatus.Created);
+    except
+      on E: exception do
+      begin
+        raise exception.Create('Erro ao inserir Sprint' + E.Message);
+        Res.Status(THTTPStatus.BadRequest);
+        oJson.AddPair('Error', 'Sprint não informado corretamente!'+sLineBreak+e.Message);
+        Res.Send<TJSONObject>(oJson);
+      end
+    end;
+  end
+  else
+  begin
+    Res.Status(THTTPStatus.BadRequest);
+    oJson.AddPair('Error', 'Sprint não encontrado!');
+    Res.Send<TJSONObject>(oJson);
+  end;
+end;
+
 class procedure TControllerBacklogSprint.Registrar(App: THorse);
 begin
   App.Post('/sprint', Post);
+  App.Post('/sprint/:id', Put);
 end;
 
 end.
